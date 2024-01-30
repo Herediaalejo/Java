f0a7baaaf933af32bf44cba0026a28a8d3a99c5e
herediaalejo <herediaalejodeveloper@gmail.com>
2024-01-29 21:48:39 -0300

    agrego ejercicio colecciones

---
+++ Proyectos/colecciones/src/ar/herediaalejo/ejemplos/set/EjemploHashSetUnicidad.java
@@ -0,0 +1,46 @@
+package ar.herediaalejo.ejemplos.set;
+
+import ar.herediaalejo.ejemplos.modelo.Alumno;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class EjemploHashSetUnicidad {
+    public static void main(String[] args) {
+
+        Set<Alumno> sa = new HashSet<>();
+        // List<Alumno> sa = new LinkedList<>();
+        sa.add(new Alumno("Pato", 5));
+        sa.add(new Alumno("Cata", 6));
+        sa.add(new Alumno("Luci", 4));
+        sa.add(new Alumno("Jano", 7));
+        sa.add(new Alumno("Andres", 3));
+        sa.add(new Alumno("Zeus2", 2));
+        sa.add(new Alumno("Zeus", 2));
+        sa.add(new Alumno("Lucas", 2));
+        sa.add(new Alumno("Lucas", 3));
+        System.out.println(sa);
+
+        /*System.out.println("Utilizando un for clásico");
+        for(int i = 0 ; i<sa.size(); i++){
+            Alumno a = sa.get(i);
+            System.out.println(a.getNombre());
+        }*/
+        
+        System.out.println("Iterando usando un foreach");
+        for(Alumno a: sa){
+            System.out.println(a.getNombre());
+        }
+
+        System.out.println("Iterando usando while y iterator");
+        Iterator<Alumno> it = sa.iterator();
+        while(it.hasNext()){
+            Alumno a = it.next();
+            System.out.println(a.getNombre());
+        }
+
+        System.out.println("Iterando usando Stream forEach");
+        sa.forEach(System.out::println);
+    }
+}
